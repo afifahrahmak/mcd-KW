3e7508d1645178ef98b3e29007f438998669025d
afifahrahmak <afifahrk96@gmail.com>
2019-10-24 18:16:16 +0700

    .

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const port = process.env.PORT || 3000;
 
 app.set("view engine", "ejs")
 app.use(express.urlencoded({ extended: true }))
-// app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
 
 app.use('/', customerRouter)
 app.use('/menu', menuRouter);
